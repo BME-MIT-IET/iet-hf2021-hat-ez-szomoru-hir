ec22f5effad21b405d171edab55a73415521194b
György Józsa <jozsagyorgy@edu.bme.hu>
2021-05-08 00:49:43 +0200

    Added dummy tests for ScientistTest.java for #14

---
+++ src/test/java/hu/grdg/projlab/model/ScientistTest.java
@@ -0,0 +1,22 @@
+package hu.grdg.projlab.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScientistTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void specialAbility() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRenderer() {
+    }
+}
