1cd8ebc9e1225d5d7c48ee423d09a506c7798273
György Józsa <jozsagyorgy@edu.bme.hu>
2021-05-08 02:07:27 +0200

    Renamed test case for clarity
    #14

--- src/test/java/hu/grdg/projlab/model/ScientistTest.java
+++ src/test/java/hu/grdg/projlab/model/ScientistTest.java
@@ -25,7 +25,7 @@ class ScientistTest {
     }
 
     @Test
-    void whenNotOnTileSpecialAbilityDoesntWork() {
+    void withoutNeighbourTileScientistSpecialAbilityDoesntWork() {
         assertFalse(scientist.specialAbility());
     }
 
