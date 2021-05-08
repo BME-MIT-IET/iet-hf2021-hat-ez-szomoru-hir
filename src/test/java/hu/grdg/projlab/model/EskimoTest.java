8bcce77cadcf52cfbff67f4e34412ce5090238c9
György Józsa <jozsagyorgy@edu.bme.hu>
2021-05-08 02:19:31 +0200

    Added EskimoTest.java
    #14

---
+++ src/test/java/hu/grdg/projlab/model/EskimoTest.java
@@ -0,0 +1,12 @@
+package hu.grdg.projlab.model;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EskimoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
