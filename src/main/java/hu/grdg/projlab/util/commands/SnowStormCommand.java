e788061ade100cbdb2a38d14330c592e24b541c0
Baaart25 <b.botond25@gmail.com>
2021-05-12 14:32:52 +0200

    removed unused import

--- src/main/java/hu/grdg/projlab/util/commands/SnowStormCommand.java
+++ src/main/java/hu/grdg/projlab/util/commands/SnowStormCommand.java
@@ -7,7 +7,6 @@ import hu.grdg.projlab.util.Command;
 import hu.grdg.projlab.util.CommandException;
 import hu.grdg.projlab.util.ProtoRuntime;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
