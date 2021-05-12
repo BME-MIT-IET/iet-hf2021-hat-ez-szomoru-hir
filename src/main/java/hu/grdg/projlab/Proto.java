9c175156dc7a460e6a82a303cbd2ff6bb68459af
Baaart25 <b.botond25@gmail.com>
2021-05-12 14:04:18 +0200

    removed not used import from Proto.java

--- src/main/java/hu/grdg/projlab/Proto.java
+++ src/main/java/hu/grdg/projlab/Proto.java
@@ -2,11 +2,8 @@ package hu.grdg.projlab;
 
 import hu.grdg.projlab.util.ProtoInputSystem;
 import hu.grdg.projlab.util.commands.*;
-import hu.grdg.projlab.util.file.GameLoadException;
-import hu.grdg.projlab.util.file.SavedGame;
 import hu.grdg.projlab.util.file.TagIO;
 
-import java.io.IOException;
 
 public class Proto {
     public static void main(String[] args) {
