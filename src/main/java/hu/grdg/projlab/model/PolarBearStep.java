9c4fa95df52a40546418a2d8a5abc38daadb602b
György Józsa <jozsagyorgy@edu.bme.hu>
2021-05-15 20:59:31 +0200

    Added comment explaining empty method
    #17

--- src/main/java/hu/grdg/projlab/model/PolarBearStep.java
+++ src/main/java/hu/grdg/projlab/model/PolarBearStep.java
@@ -4,6 +4,6 @@ public class PolarBearStep implements TurnBasedEvent{
 
     @Override
     public void doEvent(Level lvl, boolean fullTurn) {
-
+        // Do nothing, Polar bear stepping is not an event that needs side-effects
     }
 }
