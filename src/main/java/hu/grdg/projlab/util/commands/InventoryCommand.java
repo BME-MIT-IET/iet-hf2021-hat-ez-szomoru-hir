eaa194d1507ab3f4e5be9be4550f1a2458a5e1d3
György Józsa <jozsagyorgy@edu.bme.hu>
2021-05-15 06:31:55 +0200

    Removed unused import
    #17

--- src/main/java/hu/grdg/projlab/util/commands/InventoryCommand.java
+++ src/main/java/hu/grdg/projlab/util/commands/InventoryCommand.java
@@ -2,7 +2,6 @@ package hu.grdg.projlab.util.commands;
 
 import hu.grdg.projlab.ProtoIO;
 import hu.grdg.projlab.model.Item;
-import hu.grdg.projlab.model.Player;
 import hu.grdg.projlab.util.Command;
 import hu.grdg.projlab.util.CommandException;
 import hu.grdg.projlab.util.ProtoRuntime;
