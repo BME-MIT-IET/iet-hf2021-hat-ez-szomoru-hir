e318846c6bd16408256d3ffdac9ebc14babc2122
György Józsa <jozsagyorgy@edu.bme.hu>
2021-05-15 06:13:40 +0200

    Removed explicit type
    #17

--- src/main/java/hu/grdg/projlab/util/commands/AddCommand.java
+++ src/main/java/hu/grdg/projlab/util/commands/AddCommand.java
@@ -11,10 +11,10 @@ import java.util.List;
 public class AddCommand extends Command {
     @Override
     public void runCommand(ProtoRuntime state, List<String> inputParams) throws CommandException {
-        String pName = getStringArg(inputParams);
-        String type = getStringArgOf(inputParams, setOf("Rope", "DivingSuit","Food","Shovel", "BreakableShovel","Tent","RocketPart"));
+        var pName = getStringArg(inputParams);
+        var type = getStringArgOf(inputParams, setOf("Rope", "DivingSuit","Food","Shovel", "BreakableShovel","Tent","RocketPart"));
 
-        Player player = getPlayer(pName, state);
+        var player = getPlayer(pName, state);
         if(player == null) {
             ProtoIO.output(ProtoIO.OutputMessages.ERR_PLAYER_NOT_FOUND);
             return;
